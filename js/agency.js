361b69c842e414a8a91b95e6214d25cd51bc646f
sojip <sojipquantin@gmail.com>
2020-01-15 00:27:57 +0100

    ijawa bouton Read More Ok

--- js/agency.js
+++ js/agency.js
@@ -41,74 +41,34 @@
 
 })(jQuery); // End of use strict
 
+// change button text when collapse
 
-function myFunction1() {
-  var dots = document.getElementById("dots1");
-  var moreText = document.getElementById("more1");
-  var btnText = document.getElementById("myBtn1");
-
-  if (dots.style.display === "none") {
-
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-
-    
-  } else {
-   
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-  
-  }
-}
-
-// functions for read more button
-function myFunction2() {
-  var dots = document.getElementById("dots2");
-  var moreText = document.getElementById("more2");
-  var btnText = document.getElementById("myBtn2");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-} 
-
-function myFunction3() {
-  var dots = document.getElementById("dots3");
-  var moreText = document.getElementById("more3");
-  var btnText = document.getElementById("myBtn3");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-} 
-
-function myFunction4() {
-  var dots = document.getElementById("dots4");
-  var moreText = document.getElementById("more4");
-  var btnText = document.getElementById("myBtn4");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
-
+$(document).ready(function() {
+  $('[data-toggle="collapse"]').click(function() {
+    $(this).toggleClass( "active" );
+    if ($(this).hasClass("active")) {      
+      if ($(this).attr("id") == "myBtn1") {
+        $("#dots1").toggle();
+      } else if ($(this).attr("id") == "myBtn2") {
+        $("#dots2").toggle();
+      } else if ($(this).attr("id") == "myBtn3") {
+        $("#dots3").toggle();
+      } else {
+        $("#dots4").toggle();
+      }
+      $(this).text("Read Less");
+    } else {
+      if ($(this).attr("id") == "myBtn1") {
+        $("#dots1").toggle();
+      } else if ($(this).attr("id") == "myBtn2") {
+        $("#dots2").toggle();
+      } else if ($(this).attr("id") == "myBtn3") {
+        $("#dots3").toggle();
+      } else {
+        $("#dots4").toggle();
+      }
+      $(this).text("Read More");
+      
+    }
+  });
+  });
