14fb797ea8018cfc6e90e32f524cf86f60a81a59
sojip <sojipquantin@gmail.com>
2020-02-04 19:19:52 +0100

    full landing page

--- js/agency.js
+++ js/agency.js
@@ -77,3 +77,18 @@ $(document).ready(function() {
     }
   });
   });
+
+// trigger full landing page
+const $body = $("body");
+const scrollClass = "scroll";
+ 
+$(window).on("scroll", () => {
+  if (this.matchMedia("(min-width: 992px)").matches) {
+    const scrollY = $(this).scrollTop();
+    scrollY > 0
+      ? $body.addClass(scrollClass)
+      : $body.removeClass(scrollClass);
+  } else {
+    $body.removeClass(scrollClass);
+  }
+});
