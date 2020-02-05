9864774464f11d9f42d500228f312d151f794568
sojip <sojipquantin@gmail.com>
2020-02-05 13:12:08 +0100

    animation-with-css-ok

--- js/agency.js
+++ js/agency.js
@@ -92,3 +92,31 @@ $(window).on("scroll", () => {
     $body.removeClass(scrollClass);
   }
 });
+
+// Detect request animation frame
+var $animation_elements = $('.show-on-scroll');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('is-visible');
+    } else {
+      $element.removeClass('is-visible');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
